c824c60f42683a765f853b603070616458fcb13d
Gautham Vanjre <vanjregautham@gmail.com>
2023-10-24 14:39:13 +0530

    integrated tailwind and shadcn ui

--- frontend/src/Pages/Home.tsx
+++ frontend/src/Pages/Home.tsx
@@ -1,7 +1,13 @@
+import { Button } from "@/components/ui/button";
 import React from "react";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div className="text-center">
+      Hello World
+      <Button variant="secondary">Click me</Button>
+    </div>
+  );
 };
 
 export default Home;
