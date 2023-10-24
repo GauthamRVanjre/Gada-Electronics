2f4b3c9905f64933701c684fe3106180f21b82d2
Gautham Vanjre <vanjregautham@gmail.com>
2023-10-24 13:08:55 +0530

    setup react-router-dom and removed all css files

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,21 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
+import Home from "./Pages/Home";
+import Admin from "./Pages/Admin";
+import Cart from "./Pages/Cart";
+import Checkout from "./Pages/Checkout";
 
 function App() {
   return (
     <>
-      <div></div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/checkout" element={<Checkout />} />
+          <Route path="/admin" element={<Admin />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
