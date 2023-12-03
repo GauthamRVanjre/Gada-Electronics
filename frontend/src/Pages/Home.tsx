9a154c8357cda55a15b86a9e8ae31c978032510c
Gautham Vanjre <vanjregautham@gmail.com>
2023-12-03 20:20:52 +0530

    completed the sidebar work

--- frontend/src/Pages/Home.tsx
+++ frontend/src/Pages/Home.tsx
@@ -2,9 +2,25 @@
 import Navbar from "@/components/Navbar";
 import ProductsListingBar from "@/components/ProductsListingBar";
 import Sidebar from "@/components/Sidebar";
-import React from "react";
+import React, { useState } from "react";
 
 const Home: React.FC = () => {
+  const [price, setPrice] = useState([500]);
+  const [selectedCategories, setSelectedCategories] = useState<string[]>([]);
+
+  // Function to handle checkbox change
+  const handleCheckboxChange = (categoryId: string) => {
+    setSelectedCategories((prevSelectedCategories) => {
+      if (prevSelectedCategories.includes(categoryId)) {
+        // If category is already selected, remove it
+        return prevSelectedCategories.filter((id) => id !== categoryId);
+      } else {
+        // If category is not selected, add it
+        return [...prevSelectedCategories, categoryId];
+      }
+    });
+  };
+
   return (
     <div>
       {/* Navbar */}
@@ -25,12 +41,22 @@ const Home: React.FC = () => {
         {/* Sidebar */}
         {/* Sidebar (For larger screens) */}
         <div className="md:w-1/4 md:pr-8 hidden md:block">
-          <Sidebar />
+          <Sidebar
+            price={price}
+            setPrice={setPrice}
+            selectedCategories={selectedCategories}
+            handleCheckboxChange={handleCheckboxChange}
+          />
         </div>
 
         {/* Sidebar (for smaller screens) */}
         <div className="md:hidden mb-4">
-          <Sidebar />
+          <Sidebar
+            price={price}
+            setPrice={setPrice}
+            selectedCategories={selectedCategories}
+            handleCheckboxChange={handleCheckboxChange}
+          />
         </div>
 
         {/* Product Listing */}
--- frontend/src/components/Sidebar.tsx
+++ frontend/src/components/Sidebar.tsx
@@ -1,26 +1,81 @@
-const Sidebar = () => {
+import { Slider } from "@/components/ui/slider";
+import { Checkbox } from "@/components/ui/checkbox";
+import React, { Dispatch, SetStateAction } from "react";
+
+interface SidebarProps {
+  price: number[];
+  setPrice: Dispatch<SetStateAction<number[]>>;
+  selectedCategories: string[];
+  handleCheckboxChange: (categoryId: string) => void;
+}
+
+const Sidebar: React.FC<SidebarProps> = ({
+  price,
+  setPrice,
+  selectedCategories,
+  handleCheckboxChange,
+}) => {
+  // categories configuration
+  const categories = [
+    {
+      id: "smartphones",
+      label: "Smartphones",
+    },
+    {
+      id: "laptops",
+      label: "Laptops",
+    },
+    {
+      id: "lighting",
+      label: "Lightings",
+    },
+    {
+      id: "automotive",
+      label: "Automotive",
+    },
+  ] as const;
+
   return (
     <>
       <div className="mb-4">
         <h2 className="text-lg font-bold mb-2">Filter Options</h2>
         {/* Add price range filters */}
         <label className="block mb-2">
-          Price Range:
+          Price Range: Rs.{price}
           {/* Add your price range input/select components here */}
+          <Slider
+            className="mt-2"
+            defaultValue={[500]}
+            max={2000}
+            step={50}
+            value={price}
+            onValueChange={setPrice}
+          />
         </label>
       </div>
       <div>
         <h2 className="text-lg font-bold mb-2">Categories</h2>
-        {/* Add category filters */}
-        <label className="block mb-2">
-          <input type="checkbox" className="mr-2" />
-          Category 1
-        </label>
-        <label className="block mb-2">
-          <input type="checkbox" className="mr-2" />
-          Category 2
-        </label>
-        {/* Add more category options as needed */}
+
+        {categories.map((category) => {
+          return (
+            <div className="flex flex-row items-stretch mb-2">
+              <Checkbox
+                id={category.id}
+                checked={selectedCategories.includes(category.id)}
+                onCheckedChange={() => handleCheckboxChange(category.id)}
+              />
+              <div className="grid gap-1.5 leading-none">
+                <label htmlFor="terms1" className="pl-2">
+                  {category.label}
+                </label>
+              </div>
+            </div>
+          );
+        })}
+
+        <div>
+          <p>Selected Categories: {selectedCategories.join(", ")}</p>
+        </div>
       </div>
     </>
   );
